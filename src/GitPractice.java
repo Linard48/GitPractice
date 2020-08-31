bea8ec5e445373f59efba6bf37ce8e3c6eb4c9c1
Alex Irwin <alex.irwin@knights.ucf.edu>
2020-08-31 00:26:50 -0400

    revision 2 video

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -1,5 +1,6 @@
 public class GitPractice {
     public static void main(String[] args){
         System.out.println("I hope git works");
+        System.out.println("this is the second video");
     }
 }
