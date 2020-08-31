0d733514bb54403d3af8842785409629daa92f35
Alex Irwin <alex.irwin@knights.ucf.edu>
2020-08-31 03:55:08 -0400

    revision 4

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -2,5 +2,6 @@ public class GitPractice {
     public static void main(String[] args){
         System.out.println("I hope git works");
         System.out.println("this is still the second video");
+        System.out.println("this is the end of the first module");
     }
 }
